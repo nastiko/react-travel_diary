84b9c09928cb37d9715550952333f1e2ac1893cd
Anastasia <anastasia.hrynkevich@outlook.com>
2023-06-07 21:07:28 +0100

    added alt in img

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <nav>
-            <img src="./icons/globe.svg" />
+            <img src="./icons/globe.svg" alt="Icon Globe" />
             <p>TravelDiary</p>
         </nav>
     )
